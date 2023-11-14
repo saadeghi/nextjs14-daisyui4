3aa294f45d81e469523cc2a06020f7facd997567
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-14 21:22:47 +0330

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-10">
+      <button className="btn btn-primary">daisyUI 4 button</button>
+    </main>
+  );
+}
